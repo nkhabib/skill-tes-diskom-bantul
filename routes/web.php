69d4e85accd4b5f401fe35578eda8e88fc83bc81
nurkhab <wpkhabib@gmail.com>
2026-02-12 11:55:45 +0700

    soal-3-front-end

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/products');
+});
+
+Route::controller(ProductController::class)->group(function () {
+    Route::get('products', 'index');
 });
